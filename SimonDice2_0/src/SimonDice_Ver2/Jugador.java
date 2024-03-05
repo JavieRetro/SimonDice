0e9edc3bd7ab6e1730dc486a7de9cc1cdd85c3f4
Javier Olivas <javier.olivas.blanco@colegiolitterator.com>
2024-03-05 16:41:23 +0100

    Version 2 completa

---
+++ SimonDice2_0/src/SimonDice_Ver2/Jugador.java
@@ -0,0 +1,40 @@
+package SimonDice_Ver2;
+
+public class Jugador {
+	private String nombre;
+	private int puntuacion;
+	
+	public Jugador (String _nombre) {
+	this.nombre = _nombre;
+	this.puntuacion = 0;
+	}
+	/**
+	 * 
+	 * @return vble String
+	 */
+	public String getNombre() {
+		return "Hola " + this.nombre + " pulsa la tecla ENTER para empezar a jugar";	
+	}
+	/**
+	 * metodo que devuelve la vble int puntuacion
+	 * @return vble int puntuación
+	 */
+	
+	public int getPuntuacion() {
+		
+		if (this.puntuacion < 0) {
+			return this.puntuacion = 0;
+		}else {
+		return this.puntuacion;
+	}
+
+  }
+	/**
+	 * Metodo para recibir el valor de la puntuacion
+	 * @param x
+	 */
+	public void setPuntuacion(int x) {
+		
+		this.puntuacion = x;
+	}
+}
---
+++ SimonDice2_0/src/SimonDice_Ver2/Main.java
@@ -0,0 +1,17 @@
+package SimonDice_Ver2;
+import java.util.Scanner;
+/**
+ * Unica clase del programa
+ */
+public class Main {
+	/**
+	*  Punto de entrada del programa
+	* @param args
+	*/
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Engine engine = new Engine();
+		engine.start();
+	}
+}
